bebc5fcb31110d4a4213a7f9c5ab3945dc387b8d
Shehrryarr <shehrryarr@gmail.com>
2025-12-20 04:18:54 +0500

    spell corrected

--- resources/views/batches/index.blade.php
+++ resources/views/batches/index.blade.php
@@ -119,7 +119,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Edit BareCode</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Edit Barcode</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
